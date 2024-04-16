86b1f58a7ee8c6877054673ca7a4febdd62fdc3d
Bhanupratapsingh21 <bhanupss137@gmail.com>
2024-04-16 18:24:27 +0530

    added uservideo model's data models

--- src/utils/apiresponse.js
+++ src/utils/apiresponse.js
@@ -5,6 +5,5 @@ class ApiResponse {
         this.message = message
         this.success = statusCode < 400
         // read about stats code on docs yh kisse kise k hai or useage's
-         
     }
 }
